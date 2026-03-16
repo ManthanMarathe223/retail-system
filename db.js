88016b67031c5d53d3187a27435b4682a7fddf73
ManthanMarathe223 <manthanmarathe223@gmail.com>
2026-03-16 18:18:48 +0530

    feat: Implement initial database connection and setup for retail system.

--- db.js
+++ db.js
@@ -5,13 +5,18 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
+console.log("MySQL Pool created for database:", process.env.DB_NAME);
 
 db.connect((err) => {
     if (err) {
